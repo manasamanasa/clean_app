8992ee40f66eab1fd35150e4becb9b52a4551fb5
Samuel Manasse <samuelmanasse@hotmail.fr>
2012-07-19 17:57:38 +0200

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,9 @@ CleanApp::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-
+# Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
