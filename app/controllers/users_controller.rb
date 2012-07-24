2a1f8f5c24f4e1f32e2188df75c275480c522756
Samuel Manasse <samuelmanasse@hotmail.fr>
2012-07-24 11:50:55 +0200

    Finish Sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-def create
+  def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) } 
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -115,6 +116,13 @@ describe "when password is not present" do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+ end 
+    describe "remember token" do
+     before { @user.save }
+     its(:remember_token) { should_not be_blank }
   end
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
